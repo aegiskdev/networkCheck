b3dc9466536f5951b3a959d375d448e31666af7d
유치준 수석 <nerp1.cjyoo@halla.com>
2022-12-13 14:22:21 +0900

    init

---
+++ NetworkConnectivity/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.NetworkInformation;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    NetworkChange.NetworkAvailabilityChanged += NetworkChange_NetworkAvailabilityChanged;
+    Console.WriteLine("Listening for network availability changes. Press any key to exit.");
+    Console.ReadLine();
+  }
+
+  private static void NetworkChange_NetworkAvailabilityChanged(object? sender, NetworkAvailabilityEventArgs e)
+  {
+    if (e.IsAvailable)
+    {
+      Console.WriteLine("Network has become available");
+    }
+    else
+    {
+      Console.WriteLine("Network has become unavailable");
+    }
+  }
+}
---
+++ NetworkDisconnect/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    while (true)
+    {
+      Enable("Wi-Fi");
+      Console.WriteLine("Local connection ENABLED. Press key to DISABLE.");
+      Console.ReadLine();
+      Disable("Wi-Fi");
+      Console.WriteLine("Local connection DISABLED. Press key to ENABLE.");
+      Console.ReadLine();
+    }
+  }
+
+  private static void Enable(string interfaceName)
+  {
+    var psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");
+    var p = new Process { StartInfo = psi };
+    p.Start();
+  }
+
+  private static void Disable(string interfaceName)
+  {
+    var psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
+    var p = new Process { StartInfo = psi };
+    p.Start();
+  }
+}
